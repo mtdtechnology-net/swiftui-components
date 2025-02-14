ae939193820a01e504ffb20372095a90766cd2a8
Daniel Mandea <daniel.mandea@yahoo.com>
2025-02-14 16:34:05 +0200

    Done bumping up to Release 25.2.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/mtdtechnology-net/swiftui-loading-view", from: "23.10.1")
+         .package(url: "https://github.com/mtdtechnology-net/swiftui-loading-view", from: "25.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
